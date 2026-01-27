da43bd221c5ca1878df9e64503fd494eccd34d98
Kisna-Marmol <kisna.marmol2515@gmail.com>
2026-01-27 12:04:56 -0600

    Creacion de las pantallas Login, Bienvendio, Creacion de usuario

---
+++ app/src/main/java/com/example/tareamovil/CrearcionUsuarios.java
@@ -0,0 +1,14 @@
+package com.example.tareamovil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CrearcionUsuarios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crearcion_usuarios);
+    }
+}
