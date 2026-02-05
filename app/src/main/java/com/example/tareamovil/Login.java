042a4611b4e39000ceca88e5d5162edcc9ab5fc0
Kisna-Marmol <kisna.marmol2515@gmail.com>
2026-02-05 12:51:12 -0600

    Agregacion del mensaje de error al trata de ingresar sin las credenciales

--- app/src/main/java/com/example/tareamovil/Login.java
+++ app/src/main/java/com/example/tareamovil/Login.java
@@ -6,19 +6,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.tareamovil.clases.Dialog;
 
 public class Login extends AppCompatActivity {
     Button btningresar, btncrear;
+    TextView txtusuario, txtpassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        txtusuario = findViewById(R.id.txtUsuario);
+        txtpassword = findViewById(R.id.txtPassword);
         btningresar = findViewById(R.id.btnIngresar);
         btningresar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intento = new Intent(Login.this, Bienvenido.class);
-                startActivity(intento);
+                //Intent intento = new Intent(Login.this, Bienvenido.class);
+                //startActivity(intento);
+                if (txtusuario.getText().toString().trim().equals("")){
+                    Dialog.msgbox(Login.this, "Invalido", "Debe ingresar un usuario", R.drawable.error);
+                }
             }
         });
         btncrear = findViewById(R.id.btnCrearCuenta);
