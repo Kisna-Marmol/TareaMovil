cb5e668ebff77dc0750b0e8b486ba2c42ba48c4d
Kisna-Marmol <kisna.marmol2515@gmail.com>
2026-02-11 22:48:47 -0600

    proyecto listo, modificaciones en varios archivos todo funcional

--- app/src/main/java/com/example/tareamovil/CrearcionUsuarios.java
+++ app/src/main/java/com/example/tareamovil/CrearcionUsuarios.java
@@ -3,9 +3,20 @@ package com.example.tareamovil;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+
+import com.example.tareamovil.clases.ApiService;
+import com.example.tareamovil.clases.Dialog;
+
+import org.json.JSONObject;
+
 
 public class CrearcionUsuarios extends AppCompatActivity {
 
@@ -25,6 +36,96 @@ public class CrearcionUsuarios extends AppCompatActivity {
         txtUser = findViewById(R.id.txtNewUser);
         txtClave = findViewById(R.id.txtNewClave);
 
-        btnCrear = findViewById(R.id.btnCrearCuenta);
+        btnCrear = findViewById(R.id.btnCrearUsuario);
+
+        btnCrear.setOnClickListener(View -> validarCampos());
+    }
+
+    private void validarCampos()
+    {
+        if(txtUser.getText().toString().trim().equals("")||
+                txtClave.getText().toString().trim().equals("")||
+                txtApellido.getText().toString().trim().equals("")||
+                txtNombre.getText().toString().trim().equals("")||
+                txtCorreo.getText().toString().trim().equals("") ||
+                txtDNI.getText().toString().trim().equals(""))
+        {
+            Dialog.toast(CrearcionUsuarios.this,"Debe de INgresar todos los Campos");
+        }
+        else
+        {
+            crearUser();
+        }
+    }
+    private void crearUser()
+    {
+        String usuario = txtUser.getText().toString();
+        String pass = txtClave.getText().toString();
+        String apellido = txtApellido.getText().toString();
+        String nombre = txtNombre.getText().toString();
+        String correo = txtCorreo.getText().toString();
+        String dni = txtDNI.getText().toString().trim();//etDni.getText().toString().trim();
+        boolean estado=true;
+
+        ApiService.guardarUsuario(dni, nombre, apellido, correo, usuario, pass, estado, new ApiService.ApiCallback()
+                {
+                    @Override
+                    public void onSuccess(String response)
+                    {
+                        // AGREGA ESTA LÍNEA PARA VER LA RESPUESTA
+                        Log.e("CREAR_USER_RESPONSE", response);
+
+                        //progressDialog.dismiss();
+                        try {
+                            JSONObject res = new JSONObject(response);
+                            if (res.getBoolean("success"))
+                            {
+                                int idNuevo = res.getInt("id");
+                                //Toast.makeText(FormUsuarioActivity.this,
+                                //    "✅ Usuario creado exitosamente\nID: " + idNuevo,
+                                //      Toast.LENGTH_LONG).show();
+                                //Dialog.msgbox(UserActivity.this,"Exito","Usuario Guardado Satisfactoriamente",R.drawable.ok);
+                                Dialog.toast(CrearcionUsuarios.this,"Exitoso");
+                                setResult(RESULT_OK);
+                                navegarALogin();
+                                //finish();
+                            }
+                            else
+                            {
+                                String error = res.optString("error", "Error desconocido");
+                                //Toast.makeText(FormUsuarioActivity.this,
+                                //        "❌ " + error,
+                                //        Toast.LENGTH_LONG).show();
+                                //Dialog.msgbox(UserActivity.this,"Error","Error al Ingresar Usuario "+error,R.drawable.error);
+                                Dialog.toast(CrearcionUsuarios.this,"Error "+error);
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            //Toast.makeText(FormUsuarioActivity.this, "Error procesando respuesta", Toast.LENGTH_SHORT).show();
+                            //Dialog.msgbox(UserActivity.this,"Error","Error Procesando respuesta ",R.drawable.error);
+                            Dialog.toast(CrearcionUsuarios.this,"Error "+e.getMessage());
+                        }
+                    }
+                    @Override
+                    public void onError(String error)
+                    {
+                        // AGREGA ESTA LÍNEA
+                        Log.e("CREAR_USER_ERROR", error);
+                        //progressDialog.dismiss();
+                        Toast.makeText(CrearcionUsuarios.this,
+                                "❌ Error: " + error,
+                                Toast.LENGTH_LONG).show();
+                        //Dialog.msgbox(UserActivity.this,"Error","Error "+error,R.drawable.error);
+                    }
+                }
+        );
+        //Dialog.msgbox(UserActivity.this,"Información","Usuario "+nombreu+" creado Satisfactoriamente",R.drawable.ok);
+    }
+    private void navegarALogin() {
+        Intent intent = new Intent(CrearcionUsuarios.this, Login.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish(); // Cierra SplashActivity
     }
 }
--- app/src/main/java/com/example/tareamovil/Login.java
+++ app/src/main/java/com/example/tareamovil/Login.java
@@ -5,40 +5,109 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.tareamovil.clases.ApiService;
+import com.example.tareamovil.clases.Config;
 import com.example.tareamovil.clases.Dialog;
 
+import org.json.JSONObject;
 public class Login extends AppCompatActivity {
     Button btningresar, btncrear;
-    TextView txtusuario, txtpassword;
+    TextView txtUser, txtClave;
+    //@SuppressLint("MissingInflatedId")
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-        txtusuario = findViewById(R.id.txtUser);
-        txtpassword = findViewById(R.id.txtClave);
+        txtUser = findViewById(R.id.txtUsuario);
+        txtClave = findViewById(R.id.txtClave);
         btningresar = findViewById(R.id.btnIngresar);
-        btningresar.setOnClickListener(new View.OnClickListener() {
+        btncrear = findViewById(R.id.btnCrearCuenta);
+        btningresar.setOnClickListener(View -> validarCampos());
+        btncrear.setOnClickListener(View -> llamarCrearUser());
+    }
+
+    public void validarCampos()
+    {
+        if(txtUser.getText().toString().trim().equals(""))
+        {
+            //Dialog.msgbox(MainActivity.this,"Inválido","Ingrese el Usuario",R.drawable.error);
+            Dialog.toast(Login.this,"Favor Ingrese el Usuario");
+            txtUser.requestFocus();
+        }
+        else if(txtClave.getText().toString().trim().equals(""))
+        {
+            //Dialog.msgbox(MainActivity.this,"Inválido","Ingrese la Clave",R.drawable.error);
+            Dialog.toast(Login.this,"Favor Ingrese la Clave");
+            txtClave.requestFocus();
+        }
+        else
+        {
+            String usuario=txtUser.getText().toString();
+            String clave=txtClave.getText().toString();
+            confirmarCredenciales(usuario,clave);
+            //Dialog.msgbox(MainActivity.this,"Informacion","Entramos Bien",R.drawable.ok);
+        }
+    }
+    private void confirmarCredenciales(String usuario, String clave)
+    {
+        ApiService.login(usuario, clave, new ApiService.LoginCallback() {
             @Override
-            public void onClick(View view) {
-                //Intent intento = new Intent(Login.this, Bienvenido.class);
-                //startActivity(intento);
-                if (txtusuario.getText().toString().trim().equals("")){
-                    Dialog.msgbox(Login.this, "Invalido", "Debe ingresar un usuario", R.drawable.error);
+            public void onSuccess(JSONObject userdata) {
+                try
+                {
+                    boolean success=userdata.getBoolean("success");
+                    if(success)
+                    {
+                        String username = userdata.getJSONObject("data").getString("user_nombre");
+                        //txtestado.setText("✅ Bienvenido, " + username + "!");
+                        //Dialog.msgbox(MainActivity.this,"Exito","Bienvenido "+username,R.drawable.ok);
+                        Config.usuario=usuario;
+                        //navegarPral();
+                        Log.e("LOGIN EXITOSO",usuario);
+                        Dialog.toast(Login.this,"Exito");
+                        llamarPral();
+                    }
+                    else
+                    {
+                        String errorMsg = userdata.optString("error", "Credenciales inválidas");
+                        //txtestado.setText("❌ " + errorMsg);
+                        Dialog.msgbox(Login.this, "Error", errorMsg, R.drawable.error);
+                        //btnentrar.setEnabled(true);
+                        Dialog.toast(Login.this,"Error "+errorMsg);
+                    }
+                }catch (Exception e)
+                {
+                    Log.e("LOGIN_PARSE", e.getMessage());
+                    //txtestado.setText("⚠️ Error procesando respuesta");
+                    //Dialog.msgbox(MainActivity.this, "Error", "Error en la respuesta", R.drawable.error);
+                    //btnentrar.setEnabled(true);
+                    Dialog.toast(Login.this,"Error "+e.getMessage());
                 }
             }
-        });
-        btncrear = findViewById(R.id.btnCrearCuenta);
-        btncrear.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intento = new Intent(Login.this, CrearcionUsuarios.class);
-                startActivity(intento);
+            public void onError(String errorMessage) {
+                //btnentrar.setEnabled(true);
+                //txtestado.setText("❌ " + errorMessage);
+                //Toast.makeText(MainActivity.this, errorMessage, Toast.LENGTH_LONG).show();
+                //Dialog.msgbox(MainActivity.this, "Error", errorMessage, R.drawable.error);
+                Dialog.toast(Login.this,"Error "+errorMessage);
             }
         });
+    }
+    private void llamarPral()
+    {
+        Intent intent = new Intent(Login.this, Bienvenido.class);
+        startActivity(intent);
+        finish(); // Elimina el splash del stack para que no se regrese a él
+    }
+    private void llamarCrearUser()
+    {
+        Intent intent = new Intent(Login.this, CrearcionUsuarios.class);
+        startActivity(intent);
+        finish(); // Elimina el splash del stack para que no se regrese a él
     }
 }
